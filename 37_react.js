89360e65ed6f524bc4fdf9095b971838a8f9ab30
Gunn <155942588+gunnkrrish@users.noreply.github.com>
2024-10-10 20:31:09 +0530

    10 0ct

---
+++ 37_react.js
@@ -0,0 +1,8 @@
+// used to create app 
+// bina page refresh kree itni jldi kr dega
+// ek hi baar m client side m sara data ajega
+// dom ke sath sath vdom bhi bnta h 
+// pehle vdom m update hoga fr dom m
+
+
+
